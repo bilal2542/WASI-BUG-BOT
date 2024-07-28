d535f5418b4fe4e437761b12dc33eb1c0d12dd87
bilal2542 <mrawraa@gmail.com>
2024-07-28 13:30:45 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,19 +3,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923183676268"
+global.ownername = "🐛 digtal tech 2.0 "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
 global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownername = ' MR bilal ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'bilal'
 
 //console view/theme
 global.themeemoji = '🪀'
